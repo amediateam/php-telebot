e0b4897dce9b245f352a2d5c8d0db3f744f5fa46
Amirhossein Matini <matiniamirhossein@gmail.com>
2018-01-05 20:52:17 +0330

    ClosureFix

--- src/BaseHandler.php
+++ src/BaseHandler.php
@@ -2,6 +2,7 @@
 
 namespace TelegramBot\Api;
 
+use Closure;
 use ReflectionFunction;
 use ReflectionMethod;
 use TelegramBot\Api\Types\Update;
@@ -18,6 +19,9 @@ abstract class BaseHandler
             $this->callback = new ReflectionMethod($callback[0], $callback[1]);
         } else {
             $this->callback = new ReflectionFunction($callback);
+            if ($this->callback->getClosureScopeClass() === null || $this->callback->getClosureThis() !== null) {
+                Closure::bind($this->callback->getClosure(), $this->callback->getClosureThis(), $this->callback->getClosureScopeClass());
+            }
         }
     }
 
