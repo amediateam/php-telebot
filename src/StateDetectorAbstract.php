333453de51c6353bb1e2713b12f0a81c63343286
Amirhossein Matini <matiniamirhossein@gmail.com>
2018-01-15 19:13:09 +0330

    :|

--- src/StateDetectorAbstract.php
+++ src/StateDetectorAbstract.php
@@ -3,5 +3,5 @@ namespace TelegramBot\Api;
 use TelegramBot\Api\Types\Update;
 abstract class StateDetectorAbstract
 {
-    abstract public function getState(Update $update): State;
+    abstract public function getState(Update $update);
 }
